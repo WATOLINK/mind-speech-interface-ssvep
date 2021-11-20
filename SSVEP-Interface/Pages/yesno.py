c3811b36d0ddaf129278e403cbb23649906f24d6
Aaryan Shroff <aaryanshroff@v1040-wn-rt-a-100-16.campus-dynamic.uwaterloo.ca>
2021-11-20 12:44:19 -0500

    Changed Yes/No toggles from sliders to buttons

--- SSVEP-Interface/Pages/yesno.py
+++ SSVEP-Interface/Pages/yesno.py
@@ -8,41 +8,26 @@ class YesNoWindow(QtWidgets.QWidget):
     def __init__(self, parent):
         super().__init__(parent)
 
-        yes_toggle = AnimatedToggle(
-            checked_color="#03a9fc",
-            pulse_checked_color="#4403a9fc"
-        )
+        
+        yes_toggle = QtWidgets.QPushButton("Yes")
+        yes_toggle.setCheckable(True)
+        yes_toggle.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
-        no_toggle = AnimatedToggle(
-            checked_color="#03a9fc",
-            pulse_checked_color="#4403a9fc"
-        )
+        no_toggle = QtWidgets.QPushButton("No")
+        no_toggle.setCheckable(True)
+        no_toggle.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+
+        self.buttonGroup = QtWidgets.QButtonGroup()
+        self.buttonGroup.addButton(yes_toggle)
+        self.buttonGroup.addButton(no_toggle)
 
         layout = QtWidgets.QGridLayout()
 
-        yes_label = QtWidgets.QLabel('Yes')
-        yes_label.setFont(QFont('Helvetica', 24))
-        layout.addWidget(yes_label, 0, 0)
-        layout.addWidget(yes_toggle, 0, 1)
-
-        no_label = QtWidgets.QLabel('No')
-        no_label.setFont(QFont('Helvetica', 24))
-        layout.addWidget(no_label, 1, 0)
-        layout.addWidget(no_toggle, 1, 1)
-
-        yes_toggle.stateChanged.connect(functools.partial(self.yesToggleOn, yes_toggle, no_toggle))
-        no_toggle.stateChanged.connect(functools.partial(self.noToggleOn, yes_toggle, no_toggle))
+        layout.addWidget(yes_toggle, 0, 0)
+        layout.addWidget(no_toggle, 0, 1)
 
         confirm_button = QtWidgets.QPushButton('Confirm')
         confirm_button.setFont(QFont('Helvetica'))
-        layout.addWidget(confirm_button, 2, 1)
+        layout.addWidget(confirm_button, 1, 1)
 
         self.setLayout(layout)
-
-    def yesToggleOn(self, yes_toggle, no_toggle):
-        if yes_toggle.isChecked():
-            no_toggle.setCheckState(False)
-
-    def noToggleOn(self, yes_toggle, no_toggle):
-        if no_toggle.isChecked():
-            yes_toggle.setCheckState(False)
