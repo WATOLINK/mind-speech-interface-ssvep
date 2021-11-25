6efc956ba5adccdad3c77f36992023d2d8d202ee
Aaryan Shroff <aaryanshroff@v1040-wn-rt-b-82-215.campus-dynamic.uwaterloo.ca>
2021-11-24 22:21:21 -0500

    Added circle_stimuli.py

---
+++ SSVEP-Interface/Pages/circle_stimuli.py
@@ -0,0 +1,95 @@
+import sys
+from math import cos, sin, pi
+
+# https://doc.qt.io/qt-6/qopenglwidget.html 
+
+# 1. Import `QApplication` and all the required widgets
+from PyQt5.QtWidgets import (
+    QApplication,
+    QLabel,
+    QWidget,
+    QGridLayout,
+    QHBoxLayout,
+    QOpenGLWidget,
+)
+from PyQt5.QtCore import (
+    Qt,
+    QTimer
+)
+from PyQt5.QtGui import (
+    QOpenGLVersionProfile, 
+)
+import random
+
+
+class CircleFlash (QOpenGLWidget):
+    rValue = 0
+    gValue = 0
+    bValue = 0
+    opacityValue = 0
+
+    def __init__(self,freq, r, g, b,opacity):
+        super() . __init__()
+        self.flag = True
+
+        self.rValue = r/255
+        self.gValue = g/255
+        self.bValue = b/255
+        opactiyValue = opacity
+
+        timer = QTimer(self, interval=freq, timerType = 0)  # using Qt.PreciseTimer, which is accurate to 1ms
+        timer.timeout.connect(lambda: self.update()) # calls paintGL/ updates widget
+        timer.start()
+
+    def initializeGL(self):
+        # set up GL
+        version_profile  = QOpenGLVersionProfile() 
+        version_profile.setVersion(2,0)
+        self.gl = self.context().versionFunctions(version_profile)
+        self.gl.initializeOpenGLFunctions()
+
+        # depth testing 
+        self.gl.glEnable(self.gl.GL_DEPTH_TEST)
+
+    def paintGL(self):
+        if self.flag:  
+            # increase sides to get smoother edges
+            sides = 64    
+
+            # scale decides the percentage of the widget the circle occupies
+            scale = 1   
+
+            # set circle colour
+            self.gl.glColor3f(self.rValue, self.gValue, self.bValue);
+
+            # start drawing circle
+            self.gl.glBegin(self.gl.GL_POLYGON)    
+            for i in range(100):    
+                cosine = scale * cos(i * 2 * pi / sides) 
+                sine   = scale * sin(i * 2 * pi / sides)
+                self.gl.glVertex2f(cosine, sine)
+            self.gl.glEnd()    
+            
+        self.flag = not self.flag
+
+# for testing, doesnt actually do stuff when you run main
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = QWidget()
+    demo.setWindowTitle('Flashing Stim 1')
+
+    layout = QHBoxLayout()
+
+    w1 = CircleFlash(400,random.randrange(1,255), random.randrange(1,255), random.randrange(1,255), 1)
+
+    layout.addWidget(w1)
+
+
+    demo.setLayout(layout)
+    demo.resize(500, 500)
+    demo.show()
+
+    try:
+        sys.exit(app.exec_())
+    except SystemExit:
+        print('Closing Window...')
