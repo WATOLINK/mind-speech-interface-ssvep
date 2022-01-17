908c663e27ac6420096dcb11f1bac32f320d8a8b
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2022-01-16 20:27:56 -0500

    removed comments

--- SSVEP-Interface/OpenAI/main.py
+++ SSVEP-Interface/OpenAI/main.py
@@ -14,25 +14,3 @@ prompt = "Question: " + text + "?\nAnswer:"
 res = o.predictedSentences(prompt, num_results=5)
 
 print(res)
-
-# while 1:
-#    print(prompt)
-#    res = openai.Completion.create(engine="curie", max_tokens=1, temperature=1, n = 1, prompt = prompt, logprobs=3)
-#    wordDict = res["choices"][0]["logprobs"]["top_logprobs"][0]
-#    wordList = []
-#    terminalUI = ""
-   
-#    for index, key in enumerate(wordDict):
-#       terminalUI = terminalUI + str(index) + ": " + key + "\n"
-#       wordList.append(key)
-
-#    command = 5
-#    while command > 3:
-#       command = int(input(terminalUI))
-
-#    prompt += wordList[command]
-# stopCondition = [".", "!", "\n", ","]
-# print(prompt)
-# res = openai.Completion.create(engine="curie", max_tokens=10, temperature=0.1, n = 3, prompt = prompt, stop = stopCondition)
-# print(res)
-
