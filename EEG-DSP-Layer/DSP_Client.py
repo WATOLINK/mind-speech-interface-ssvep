8292f5adb494f3da0481b82b50a2de66e1e2c991
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2022-01-28 19:28:45 -0500

    fixed bugs

--- EEG-DSP-Layer/DSP_Client.py
+++ EEG-DSP-Layer/DSP_Client.py
@@ -1,3 +1,4 @@
+from sre_constants import SUCCESS
 import numpy as np
 import pandas as pd
 import socket
@@ -24,12 +25,15 @@ class EEGSocketListener:
     def __init__(self, host='127.0.0.1', port=65432, num_channels=16, input_len=125, output_size=5):
         self.host = host
         self.port = port
+
         self.input_len = input_len
         self.output_size = output_size
         self.num_channels = num_channels
 
-        self.data = np.empty((num_channels, output_size*input_len))
+        self.data = np.empty((output_size*input_len, num_channels))
         self.samples = 0
+
+
 
     def open_socket_conn(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,26 +42,30 @@ class EEGSocketListener:
     def recieve_packet(self):
         # the size of the input data = num elements * 8 bytes + 500 for leeway
         sample = self.socket.recv(self.input_len * self.num_channels * 8 + 500)
-        # If sample is recevied
-        if sample: 
-            sample = pickle.loads(sample)
-            assert type(sample) == np.ndarray,  "Not a Numpy ND Array"
-            assert sample.shape == (self.input_len, self.num_channels), "Incorrect Shape"
-            return sample
-        else:
+        if sample == None:
             print("Recieved nothing")
-            return None
+        else: # If not null sample is recevied
+            sample = pickle.loads(sample)
+            assert type(sample) == np.ndarray,  f"Not a Numpy ND Array {type(sample), sample}"
+            assert sample.shape == (self.input_len, self.num_channels), \
+                f"Incorrect Shape, Expected: {(self.input_len, self.num_channels)}, Recieved: {sample.shape}"
+        return sample
 
     def listen(self):
         while True:
-            egg = self.recieve_packet()
-            if egg:
+            packet = self.recieve_packet()
+            if packet.any():
                 start = self.input_len * self.samples 
                 end = self.input_len * (self.samples + 1)
-                self.data[:, start:end] = egg
-                samples = (samples + 1) % self.output_size
+                self.data[start:end, :] = packet
+                print(f"SUCCESS {self.samples+1}/{self.output_size}")
+                del packet
+                self.samples = (self.samples + 1) % self.output_size
+                if self.samples == 0:
+                    # TO IMPLEMENT
+                    print("OUTPUT BUFFER FILLED, SEND DATA TO AI")
             
-    def generate_csv(self, name="fullOBCI"):
+    def generate_csv(self, data, name="fullOBCI"):
         df = pd.DataFrame(data=data, columns=list(range(1,17)))
         print(f'{name}.csv Generated')
         df.to_csv(f'{name}.csv')
@@ -73,7 +81,9 @@ if __name__ == '__main__':
     parser.add_argument('--num-channels', type=int, help='number of columns in input array', 
         required=False, default=16)
     parser.add_argument('--output-size', type=int, help='number of samples needed to fill output array', 
-        quired=False, default=5)
+        required=False, default=5)
     args = parser.parse_args()
 
-    listener = EEGSocketListener(args.host, args.port, args.input_len, args.num_channels, args.output_size)
+    listener = EEGSocketListener(args.host, args.port, args.num_channels, args.input_len, args.output_size)
+    listener.open_socket_conn()
+    listener.listen()
