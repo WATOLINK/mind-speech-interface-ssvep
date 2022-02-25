73880ee018df402f18474ac3a242425f96952766
dpatel <dpatel@flinks.io>
2022-02-24 21:05:30 -0500

    Updates for training and testing via cli

--- EEG-Data/models/CCAKNN.py
+++ EEG-Data/models/CCAKNN.py
@@ -8,10 +8,12 @@ from sklearn.cross_decomposition import CCA
 import pickle
 import pandas as pd
 from joblib import dump, load
+import argparse
 
 # Helper functions
 from .ssvep_utils import get_filtered_eeg, get_segmented_epochs
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+
 
 class CCAKNNModel:
     """
@@ -25,22 +27,23 @@ class CCAKNNModel:
         - train
         - test and develop metrics
     """
-    def __init__(self, model_path: os.PathLike, sample_rate: int = 125, window_len: int = 1, shift_len: int = 1, data: pd.DataFrame = None, left_freq: float = 5, right_freq: float = 21, **kwargs):
 
-        self.window_len = window_len
-        self.shift_len = shift_len
-        self.sample_rate = sample_rate
-        self.duration = self.window_len * self.sample_rate
+    def __init__(self):
+
+        # self.window_len = window_len
+        # self.shift_len = shift_len
+        # self.sample_rate = sample_rate
+        # self.duration = self.window_len * self.sample_rate
 
         # possible flicker frequencies shown to the user
         self.flicker_freq = np.array([9.25, 11.25, 13.25, 9.75, 11.75, 13.75, 
                             10.25, 12.25, 14.25, 10.75, 12.75, 14.75])
         # self.flicker_freq = np.arange(left_freq, right_freq)
 
-        self.KNN = self.load_model(model_path)
+        # self.KNN = self.load_model(model_path)
     
     def load_model(self, model_path: os.PathLike):
-        with open(model_path,'rb') as f:
+        with open(model_path, 'rb') as f:
             return pickle.load(f)
     
     def save_model(self, filepath):
@@ -60,7 +63,7 @@ class CCAKNNModel:
         
     def get_reference_signals(self, data_len, target_freq, sampling_rate):
         reference_signals = []
-        t = np.arange(0, (data_len/(sampling_rate)), step=1.0/(sampling_rate))
+        t = np.arange(0, (data_len/sampling_rate), step=1.0/sampling_rate)
         reference_signals.append(np.sin(np.pi*2*target_freq*t))
         reference_signals.append(np.cos(np.pi*2*target_freq*t))
         reference_signals.append(np.sin(np.pi*4*target_freq*t))
@@ -69,19 +72,75 @@ class CCAKNNModel:
 
         return reference_signals
 
-    def calculate_correlation(signals: np.array, reference: np.array, n_components=1):
+    def calculate_correlation(self, signals: np.array, reference: np.array, n_components=1) -> np.array:
         """
         Find correlation between input `signals` and `reference` signals using CCA.
 
         Args:
             signals: The input ndarray of signals
             reference: The reference signals
+            n_components: The number of components for CCA.
+
+        Returns:
+            Correlations to each of the reference signals
         """
+
         cca = CCA(n_components=n_components)
         result = np.zeros(reference.shape[0])
         for timestep in range(reference.shape[0]):
             cca.fit(signals.T, np.squeeze(reference[timestep, :, :]).T)
-            Xcorr, Ycorr = cca.transform(signals.T, np.squeeze(reference[timestep, :, :]).T)
-            result[timestep] = np.max(np.corrcoef(Xcorr[:, 0], Ycorr[:, 0])[0, 1])
+            x_corr, t_corr = cca.transform(signals.T, np.squeeze(reference[timestep, :, :]).T)
+            result[timestep] = np.max(np.corrcoef(x_corr[:, 0], y_corr[:, 0])[0, 1])
         return result
     
+    def train(self, hparams, train_data, train_labels):
+        """"""
+        clf = sklearn.neighbors.KNeighborsClassifier(hparams.neighbors)
+        clf.fit(train_data, train_labels)
+        self.KNN = clf
+    
+    def test(self, hparams, test_data, test_labels):
+        """"""
+        predictions = self.KNN.predict(test_data)
+        print(f'accuracy: {accuracy_score(test_labels, predictions):.4f}')
+        print(f'precision: {precision_score(test_labels, predictions):.4f}')
+        print(f'recall: {recall_score(test_labels, predictions):.4f}')
+        if hparams.verbose:
+            print(confusion_matrix(test_labels, predictions))
+
+def get_args(parser):
+    parser.add_argument('--neighbours', type=int, default=15, help="The number of neighbours to pass to a KNN")
+    parser.add_argument('--training-data', type=os.PathLike, help="Filepath for the training data (csv)")
+    parser.add_argument('--testing-data', type=os.PathLike, help="Filepath for the testing data (csv)")
+    parser.add_argument('--model-path', type=os.PathLike, help="Filepath for a trained KNN model")
+    parser.add_argument('--sample-rate', type=int, default=250, help="Sampling rate (hz)")
+    parser.add_argument('--window-len', type=int, default=1, help="Window length for data processing")
+    parser.add_argument('--shift-len', type=int, default=1, help="Shift length for data processing")
+    parser.add_argument('--lower-freq', type=float, default=5, help="Lower frequency bound for a bandpass filter")
+    parser.add_argument('--upper-freq', type=float, default=5, help="Upper frequency bound for a bandpass filter")
+
+    return parser.parse_args()
+
+def train(hparams, model):
+    train_data = pd.read_csv(hparams.training_data)
+
+    model.train(train_data, train_labels)
+
+def test(hparams, model):
+    test_data = pd.read_csv(hparams.testing_data)
+
+    model.test(test_data, test_labels)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args, _ = get_args(parser)
+
+    model = CCAKNNModel()
+    if args.model_path:
+        model.load_model(args.model_path)
+    
+    if args.training_data:
+        train(args, model)
+
+    if args.testing_data:
+        test(args, model)
