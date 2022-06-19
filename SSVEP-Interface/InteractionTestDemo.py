b9d34ebad4a081852fd7a10056a1cfec40dd36fd
Ivan <ivanyu327@gmail.com>
2022-06-19 14:38:37 -0400

    redid YN interaction test demo structure and made it work lol

--- SSVEP-Interface/InteractionTestDemo.py
+++ SSVEP-Interface/InteractionTestDemo.py
@@ -1,15 +1,14 @@
 import sys
-from PyQt5.QtCore import center, Qt
-from PyQt5.QtGui import QFont
 
-from PyQt5.QtWidgets import QApplication, QStackedWidget
+from PyQt5.QtWidgets import QApplication,QVBoxLayout,QWidget,QHBoxLayout,QSizePolicy,QLineEdit,QLabel
 from PyQt5.QtWidgets import QMainWindow
 
-# from Pages.QAPage.qa import QuestionAndAnswerWidget
-# from Pages.MCPage.mc import MultipleChoiceWidget
-from InteractionTest.absolute import TrueAndFalseWidget
+from Pages.styles import windowStyle,textBoxStyle
+from Pages.button_container import ButtonContainer
 
-from Pages.styles import windowStyle
+from Pages.HomePage.homepage import promptBox
+
+from InteractionTest.yesno import YesNoWidget
 
 
 class Window(QMainWindow):
@@ -18,50 +17,66 @@ class Window(QMainWindow):
     def __init__(self, parent=None):
         """Initializer."""
         super().__init__(parent)
-        # Make sure to pass in 'self' to the child widget for it to access parent methods
-        # self.multiplePage = MultipleChoiceWidget(self)
-        # self.questionPage = QuestionAndAnswerWidget(self)
-        self.trueFalsePage = TrueAndFalseWidget(self)
-        # We use the Stack Widget to navigate between different pages
-        # If new pages need to be added, import it to this file and add it to the stack
-        self.stacked = QStackedWidget()
-        # self.stacked.addWidget(self.questionPage)
-        # self.stacked.addWidget(self.multiplePage)
-        self.stacked.addWidget(self.trueFalsePage)
+
+
         self.setWindowTitle('Main Window')  # Sets name of window
         # Adds central widget where we are going to do most of our work
-        self.setCentralWidget(self.stacked)
+
+        self.setCentralWidget(HomePageWidget(self))
+
         # Sets location (x, y) and size (width, height) of current window
         self.setGeometry(0, 0, 1600, 900)
 
-    
 
-    # To navigate to different pages, we set the current widget of the stack
-    # These function below are called to nagivate between different pages
-    # def showQA(self):
-    #     self.stacked.setCurrentWidget(self.questionPage)
 
-    # def showMC(self):
-    #     self.stacked.setCurrentWidget(self.multiplePage)
+class HomePageWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__()
 
-    def showTF(self):
-        self.stacked.setCurrentWidget(self.trueFalsePage)
+        layout = QVBoxLayout()
+        
+        
+        layout.addWidget(promptBox("ඞ"))
+        layout.addWidget(inputBox(self))
+        layout.addWidget(YesNoWidget(self))
+        
+        bottomLayout = QHBoxLayout()
+        bottomLayout.setStretch(0, 1)
+        bottomLayout.setStretch(1, 1)
+        bottomLayout.setStretch(2, 1)
+        
+        confirmButton = ButtonContainer("Confirm", horizontal=True, checkable=False, border=False)
+        confirmButton.clicked.connect(lambda: confirm(parent))
 
-    # Functions below may be useful in the future
-    # def _createMenu(self):
-    #     self.menu = self.menuBar().addMenu("&Menu")
-    #     self.menu.addAction('&Exit', self.close)
+        self.dummyWidget = QWidget()
+        self.dummyWidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        bottomLayout.addWidget(self.dummyWidget) # Dummy widget
+        bottomLayout.addWidget(confirmButton)
+        bottomLayout.addWidget(self.dummyWidget) # Dummy widget
 
-    # def _createToolBar(self):
-    #     tools = QToolBar()
-    #     self.addToolBar(tools)
-    #     tools.addAction('Exit', self.close)
+        layout.addLayout(bottomLayout)
 
-    # def _createStatusBar(self):
-    #     status = QStatusBar()
-    #     status.showMessage("I'm the Status Bar")
-    #     self.setStatusBar(status)
+        self.setLayout(layout)
 
+def inputBox(parent):
+    textbox = QLineEdit()
+    textbox.setStyleSheet(textBoxStyle)
+    textbox.setObjectName("Input")
+    return textbox
+
+def confirm(parent):
+    messageBox = parent.findChild(QLabel,"Prompt")
+    inputField = parent.findChild(QLineEdit,"Input")
+    currWidget = parent.findChild(QWidget, "YN Widget")
+
+    if inputField.text():
+        temp = messageBox.text() + f"[{inputField.text()}]"
+        messageBox.setText(temp)
+        inputField.clear()
+
+    for button in currWidget.findChildren(ButtonContainer):
+        if button.isChecked():
+            button.setChecked(False)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
