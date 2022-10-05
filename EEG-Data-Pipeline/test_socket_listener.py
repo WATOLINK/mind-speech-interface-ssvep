52f3624c89c259c7704665e28902001e25fbdc2b
Dhrumil Patel <dhrumilp15@gmail.com>
2022-10-04 20:41:39 -0400

    Reset files and remove model files

--- EEG-Data-Pipeline/test_socket_listener.py
+++ EEG-Data-Pipeline/test_socket_listener.py
@@ -1,6 +1,12 @@
 import socket
 import pickle
 
+def run_tsl(app, synch, q):
+    app.open_socket_conn()
+    synch.wait()
+    app.run()
+    # if q.get() is None:
+    #     app.close_socket_conn()
 
 class TestSocketListener:
     def __init__(self):
@@ -9,8 +15,8 @@ class TestSocketListener:
     def open_socket_conn(self):
         try:
             self.s.connect(('127.0.0.1', 55432))
-        except Exception as e:
-            print(f"Couldn't connect to 127.0.0.1:55432, {e}")
+        except:
+            pass
 
     def close_socket_conn(self):
         self.s.close()
@@ -20,17 +26,6 @@ class TestSocketListener:
             try:
                 msg = self.s.recv(1250 * 8 * 8 + 5000000)
                 message = pickle.loads(msg)
-                print(f"App received: {message}")
-            except KeyboardInterrupt:
-                break
-            except Exception as e:
-                print(f"Couldn't listen, raised: {e}")
+                print(message)
+            except:
                 self.open_socket_conn()
-        self.close_socket_conn()
-
-
-def run_tsl(synch):
-    app = TestSocketListener()
-    app.open_socket_conn()
-    synch.wait()
-    app.run()
