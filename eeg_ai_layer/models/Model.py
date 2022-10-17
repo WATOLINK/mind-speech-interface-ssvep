6e94ef93176c594466ccfb5576f4530c426ca631
Dhrumil Patel <dhrumilp15@gmail.com>
2022-10-17 15:47:25 -0400

    Build FBCCA-KNN, impl dynamic prediction, train a new fbcca-knn model

--- eeg_ai_layer/models/Model.py
+++ eeg_ai_layer/models/Model.py
@@ -1,5 +1,7 @@
 from eeg_ai_layer.models.CCAKNN import CCAKNNModel
 from eeg_ai_layer.models.FBCCA import FBCCA
+from eeg_ai_layer.models.KNN import KNN
+
 
 def load_model(args):
     """
@@ -9,3 +11,5 @@ def load_model(args):
         return CCAKNNModel(args)
     if args.model_type == 'fbcca':
         return FBCCA(args)
+    if args.model_type == 'fbcca_knn':
+        return KNN(args)
